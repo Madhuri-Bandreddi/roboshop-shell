c592e6129f188f38c8f49cbdba8681f6bfcb3dbe
mammu <bandreddymammu599@gmail.com>
2025-01-24 23:33:15 +0530

    update catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -13,16 +13,15 @@ cd /app
 echo -e "\e[36m >>>>>> unzip app content <<<<<<\e[0m"
 unzip /tmp/catalogue.zip
 echo -e "\e[36m >>>>>> Install Nodejs dependencies <<<<<<\e[0m"
-cd /app
 npm install
 echo -e "\e[36m >>>>>> Copy Catalogue Service file <<<<<<\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service
 echo -e "\e[36m >>>>>> start catalogue service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 echo -e "\e[36m >>>>>> copy mongodb repo <<<<<<\e[0m"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m >>>>>> Install mongodb client <<<<<<\e[0m"
 dnf install mongodb-org-shell -y
 echo -e "\e[36m >>>>>> Load schema <<<<<<\e[0m"
