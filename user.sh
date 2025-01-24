286f0937b2ce027563db8217cc7b6a942e58247f
mammu <bandreddymammu599@gmail.com>
2025-01-25 00:38:42 +0530

    update mysql

--- user.sh
+++ user.sh
@@ -21,10 +21,10 @@ unzip /tmp/user.zip
 echo -e "\e[36m >>>>>> Install Nodejs dependencies <<<<<<\e[0m"
 npm install
 
-echo -e "\e[36m >>>>>> Copy Catalogue Service file <<<<<<\e[0m"
+echo -e "\e[36m >>>>>> Copy user Service file <<<<<<\e[0m"
 cp /root/roboshop-shell/user.service /etc/systemd/system/user.service
 
-echo -e "\e[36m >>>>>> start catalogue service <<<<<<\e[0m"
+echo -e "\e[36m >>>>>> start user service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
