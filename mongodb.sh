aef235cc8e9faf1b3ad07aef0f9a7028f0107206
mammu <bandreddymammu599@gmail.com>
2025-01-24 22:54:20 +0530

    update mongod

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
-cp mongod.service /etc/yum.repos.d/mongod.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.service
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
 systemctl restart mongod
