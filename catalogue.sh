b5dcde4ce05d2a66a8c4e72293bab8879a304781
mammu <bandreddymammu599@gmail.com>
2025-01-24 23:42:08 +0530

    update catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,28 +1,39 @@
 echo -e "\e[36m >>>>>> configuring Nodejs repos <<<<<<\e[0m"
 curl -sl https://rpm.nodesource.com /setup_lts.x | bash
+
 echo -e "\e[36m >>>>>> Install Nodejs <<<<<<\e[0m"
 dnf install nodejs -y
+
 echo -e "\e[36m >>>>>> Add Application user <<<<<<\e[0m"
 useradd roboshop
+
 echo -e "\e[36m >>>>>> Create application directory <<<<<<\e[0m"
 rm -rf /app
 mkdir /app
+
 echo -e "\e[36m >>>>>> Download app content <<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+
 echo -e "\e[36m >>>>>> unzip app content <<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+
 echo -e "\e[36m >>>>>> Install Nodejs dependencies <<<<<<\e[0m"
 npm install
+
 echo -e "\e[36m >>>>>> Copy Catalogue Service file <<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
 echo -e "\e[36m >>>>>> start catalogue service <<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
 echo -e "\e[36m >>>>>> copy mongodb repo <<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
 echo -e "\e[36m >>>>>> Install mongodb client <<<<<<\e[0m"
 dnf install mongodb-org-shell -y
+
 echo -e "\e[36m >>>>>> Load schema <<<<<<\e[0m"
 mongo --host monogdb.madhari123.shop </app/schema/catalogue.js
