e3f6dc350e8369656ea2ee6970983b7c6c3c2f5c
mammu <bandreddymammu599@gmail.com>
2025-01-31 22:05:35 +0530

    update catalogue1

--- frontend.sh
+++ frontend.sh
@@ -28,11 +28,11 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 func_stat_check $?
 
 func_print_head "Extracting App Content"
-cd /usr/share/nginx/html &>>$log file
+cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
 func_stat_check $?
 
 func_print_head "Start Nginx"
-systemctl enable nginx &>>$log file
-systemctl restart nginx &>>$log file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
 func_stat_check $?
