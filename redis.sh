2cbc0d76a06783454b20a3095f3528f302ee7987
mammu <bandreddymammu599@gmail.com>
2025-01-31 23:03:12 +0530

    update catalogue1

--- redis.sh
+++ redis.sh
@@ -16,14 +16,14 @@ func_stat_check $?
 
 func_print_head "Install Redis"
 dnf module enable redis:remi-6.2 -y &>>$log_file
-yum install redis -y &>>$log file
+yum install redis -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Update Redis Listen Address"
-sed -i -e 's|127.0.0.1|0.0.0.0| ' /etc/redis.conf /etc/redis/redis.conf &>$log file
+sed -i -e 's|127.0.0.1|0.0.0.0| ' /etc/redis.conf /etc/redis/redis.conf &>$log_file
 func_stat_check $?
 
 func_print_head "Start Redis Service"
-systemctl enable redis &>>$log file
-systemctl restart redis &>>$log file
+systemctl enable redis &>>$log_file
+systemctl restart redis &>>$log_file
 func_stat_check $?
