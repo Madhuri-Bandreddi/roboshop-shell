4a2e2e2b233b5e028a05a91f67d6e6ca52caea1e
mammu <bandreddymammu599@gmail.com>
2025-01-31 23:17:09 +0530

    update catalogue1

--- mongodb.sh
+++ mongodb.sh
@@ -7,14 +7,14 @@
 
 script=$(realpath "$0")
 script_path=$(dirname "$script")
-source ${script_path}/common. sh
+source ${script_path}/common.sh
 
 func_print_head "Setup MongoDB Repo"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 func_stat_check $?
 
 func_print_head "Install MongoDB"
-yum install mongodb-org -y &>>$log file
+yum install mongodb-org -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Update MongoDB Listen Address"
@@ -22,6 +22,6 @@ sed -i -e 's| 127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
 func_stat_check $?
 
 func_print_head "Start MongoDB"
-systemctl enable mongod &>>$log file
-systemctl restart mongod &>>$log file
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
 func_stat_check $?
