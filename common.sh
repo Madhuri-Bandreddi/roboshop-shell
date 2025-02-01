708cf513bd9bd785df10e979af91a01043a63410
mammu <bandreddymammu599@gmail.com>
2025-02-01 16:40:37 +0530

    update catalogue1

--- common.sh
+++ common.sh
@@ -1,24 +1,48 @@
-#app_user=roboshop
-#script=$(realpath "$0")
-#script_path=$(dirname "$script")
-#log_file=/tmp/roboshop.log
-#rm -f $log_file
-#
-#func_print_head() {
-#  echo -e " \e[35m>>>>>>>>> $1 <<<<<<<< \e[0m"
-#  echo -e "\e[35m>>>>>>>>> $1 <<<<<<<< \e[0m" &>>$log_file
-#}
-#
-#func_stat_check() {
-#  if [ $1 -eq 0 ]; then
-#    echo -e "\e[32mSUCCESS\e[Øm"
-#  else
-#    echo -e "\e[31mFAILURE\e[Øm"
-#    echo "Refer the log file /tmp/roboshop.log for more information"
-#    exit 1
-#  fi
-#}
-#
+app_user=roboshop
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+log_file=/tmp/roboshop.log
+# rm -f $log_file
+
+func_print_head(){
+  echo -e "\e[36m >>>>>>>> $* <<<<"
+  echo -e "\e[36m >>>>>>>> $* <<<<" &>>$log_file
+}
+func_stat_check(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[33mFALIURE\e[0m"
+    echo "Refer the log file /tmp/roboshop.log for more information"
+    exit 1
+  fi
+}
+
+func_schema_setup(){
+ if [ "$ schema_setup" == "mongo" ] ; then
+   func_print_head "Copy MongoDB repo"
+   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+   func_stat_check $?
+
+   func_print_head Install  "MongoDB Client"
+   dnf install mongodb-org-shell -y &>>$log_file
+   func_stat_check $?
+
+   func_print_head "Load Schema "
+   mongo --host mongodb.madhari123.shop </app/schema/${component} &>>$log_file
+   func_stat_check $?
+ fi
+ if [ "${schema_setup}" == "mysql" ]; then
+   func_print_head  "INSTALL MYSQL client"
+   dnf install mysql -y &>>$log_file
+   func_stat_check $?
+
+   func_print_head  "Load Schema"
+   mysql -h mysql.madhari123.shop  -uroot -p$mysql_root_password < /app/schema/${component}.sql &>>$log_file
+   func_stat_check $?
+ fi
+
+}
 #func_schema_setup() {
 #  if [ "$schema_setup" == "mongo" ]; then
 #    func_print_head "Copy MongoDB repo"
@@ -128,51 +152,6 @@
 #  func_systemd_setup
 #}
 
-app_user=roboshop
-script=$(realpath "$0")
-script_path=$(dirname "$script")
-log_file=/tmp/roboshop.log
-# rm -f $log_file
-
-func_print_head(){
-  echo -e "\e[36m >>>>>>>> $* <<<<"
-  echo -e "\e[36m >>>>>>>> $* <<<<" &>>$log_file
-}
-func_stat_check (){
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[33mFALIURE\e[0m"
-    echo "Refer the log file /tmp/roboshop.log for more information"
-    exit 1
-  fi
-}
-
-func_schema_setup(){
- if [ "$ schema_setup" == "mongo" ] ; then
-   func_print_head "Copy MongoDB repo"
-   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
-   func_stat_check $?
-
-   func_print_head Install  "MongoDB Client"
-   dnf install mongodb-org-shell -y &>>$log_file
-   func_stat_check $?
-
-   func_print_head "Load Schema "
-   mongo --host mongodb.madhari123.shop </app/schema/${component} &>>$log_file
-   func_stat_check $?
- fi
- if [ "${schema_setup}" == "mysql" ]; then
-   func_print_head  "INSTALL MYSQL client"
-   dnf install mysql -y &>>$log_file
-   func_stat_check $?
-
-   func_print_head  "Load Schema"
-   mysql -h mysql.madhari123.shop  -uroot -p$mysql_root_password < /app/schema/${component}.sql &>>$log_file
-   func_stat_check $?
- fi
-
-}
 
 func_app_prereq(){
   func_print_head "Create Application User"
