208bf3a6a5a084ecdeb9139f098a380c3aa86a08
mammu <bandreddymammu599@gmail.com>
2025-02-01 13:30:12 +0530

    update catalogue1

--- mysql.sh
+++ mysql.sh
@@ -17,7 +17,7 @@ if [ -z "$mysql_root_password" ]; then
 fi
 
 func_print_head "Disable MySQL 8 Version"
-dnf module disable mysql -y &>>$log_file
+yum module disable mysql -y &>>$log_file
 func_stat_check $?
 
 func_print_head "Copy MySQL Repo File"
