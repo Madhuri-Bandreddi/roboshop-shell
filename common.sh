d0d06b29f4f690d85e103db57ed2d5df3f24822c
mammu <bandreddymammu599@gmail.com>
2025-02-01 21:33:56 +0530

    update catalogue1

--- common.sh
+++ common.sh
@@ -4,11 +4,11 @@ script_path=$(dirname "$script")
 log_file=/tmp/roboshop.log
 # rm -f $log_file
 
-func_print_head(){
+func_print_head() {
   echo -e "\e[36m >>>>>>>> $* <<<<"
   echo -e "\e[36m >>>>>>>> $* <<<<" &>>$log_file
 }
-func_stat_check (){
+func_stat_check () {
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
@@ -18,7 +18,7 @@ func_stat_check (){
   fi
 }
 
-func_schema_setup(){
+func_schema_setup() {
  if [ "$schema_setup" == "mongo" ] ; then
    func_print_head "Copy MongoDB repo"
    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
@@ -41,10 +41,9 @@ func_schema_setup(){
    mysql -h mysql.madhari123.shop  -uroot -p$mysql_root_password < /app/schema/${component}.sql &>>$log_file
    func_stat_check $?
  fi
-
 }
 
-func_app_prereq(){
+func_app_prereq() {
   func_print_head "Create Application User"
   id ${app_user} &>>/tmp/roboshop.log
   if [ $? -ne 0 ]; then
@@ -68,7 +67,7 @@ func_app_prereq(){
   func_stat_check $?
 }
 
-func_systemd_setup(){
+func_systemd_setup() {
   func_print_head "Setup Systemd Service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service &>>$log_file
   func_stat_check $?
@@ -80,27 +79,27 @@ func_systemd_setup(){
   func_stat_check $?
 
 }
-func_nodejs(){
-  func_print_head  "configuring nodejs  repos"
+
+func_nodejs() {
+  func_print_head "Configuring Node.js Repos"
   curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash - &>>$log_file
   func_stat_check $?
 
-  func_print_head  "Install Nodejs  repos"
+  func_print_head "Install Node.js"
   dnf install nodejs -y &>>$log_file
   func_stat_check $?
 
   func_app_prereq
 
-  func_print_head  "Install   NodeJS  Dependencies"
+  func_print_head "Install NodeJS Dependencies"
   npm install &>>$log_file
   func_stat_check $?
 
   func_schema_setup
   func_systemd_setup
-
 }
 
-func_java(){
+func_java() {
   func_print_head "Install Maven"
   yum install maven -y &>>$log_file
   func_stat_check $?
